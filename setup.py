8f9b0b9ed15150be9af67b218e8109106ad7ac56
paulspray <paul.spray@cqumail.com>
2024-04-25 10:39:03 +1000

    unused workspace removed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CQU unit assessment for COIT12213',
+    author='Paul Spray S12084819',
+    license='MIT',
+)
